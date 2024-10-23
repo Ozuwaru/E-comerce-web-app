3b5b703e786e64662601afc8f07afa2d7024d9b5
unknown <oswaldtorrealba1@gmail.com>
2024-10-22 21:03:16 -0400

    initial save

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Avatar} from "@nextui-org/react";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold ">
+        Hello world!
+      </h1>
+
+    </>
+  )
+}
+
+export default App
